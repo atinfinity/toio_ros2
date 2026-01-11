74a4a914a0244f9cb3b8ff0c15b16cf1948bd522
atinfinity <dandelion1124@gmail.com>
2026-01-11 09:31:10 +0900

    add pameter files

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*launch.[pxy][yma]*')),
+        (os.path.join('share', package_name, 'params'), glob(os.path.join('params', '*.yaml'))),
         (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz'))),
     ],
     install_requires=['setuptools'],
